5ea06e6bc4856d3dba32a643bb7772a04059d946
Karl-Espindola <100312583+Karl-Espindola@users.noreply.github.com>
2022-02-24 15:01:54 -0500

    Add files via upload
    Cuenta con diseño responsive para visualizarse en dispositivos móviles

---
+++ js/main.js
@@ -0,0 +1,8 @@
+
+const bars = document.querySelector('.bars');
+const menu = document.querySelector('.menu');
+
+bars.addEventListener('click',function activar(){
+	menu.classList.toggle('activar');
+
+});
